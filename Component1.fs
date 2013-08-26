e4dea69441b77601bba308f9bf5b63135f5666d0
nagaco <sfault@gmail.com>
2013-08-26 22:18:11 +0900

    updated

--- Component1.fs
+++ Component1.fs
@@ -5,13 +5,22 @@
 
 module AutoCity.BuildCity
 
+type store_tag = GroceryStore
+               | BookStore
+               | FoodStore
+
+type building_tag = House
+                  | Depart
+                  | School
+                  | Store of store_tag
+
+type building_property = {name : string; tag : building_tag}
+
 type city_tile_t =
     | Road
-    | Buliding
-    | House
-    | Shop
+    | Buliding of building_property
     | Empty
-    
+                                                
 type city_tiles_t = {Tiles : city_tile_t [,]; Width : int; Height : int}
 
 let genCityTiles w h : city_tiles_t = {Tiles = Array2D.create w h Empty; Width = w; Height = h}
@@ -32,12 +41,10 @@ let cityTile2String t =
     match t with
     | Empty -> "E"
     | Road -> "R"
-    | _ -> " "
+    | _ -> " "   
 
 let printAr2Di a i = Array.iter (fun j -> Array2D.get a i j |> printf "%s") [|0 .. Array2D.length2 a - 1|]; printfn ""
 
 let printAr2D a = Array.iter (printAr2Di a) [|0 .. Array2D.length1 a - 1|]
                                     
 let main () = genCityTiles 10 10 |> putRoads 3 2 |> fun city -> Array2D.map cityTile2String city.Tiles |> printAr2D
-
-main()
